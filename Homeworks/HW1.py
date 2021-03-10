a3bc49b1d71bf834958dd298f1d9ec9ee4ba478b
Cemal Dağlı <43971035+cemaldagli@users.noreply.github.com>
2021-03-10 14:39:31 +0100

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,44 @@
-#Explain your work
+def mergeListsHere(list1,list2):
 
-#Question 1
-for x in range(a):
-	print(a)
+    return list1+list2
+
+def multiplyMergedList(mergedList):
+
+    for i in range(0,len(mergedList)):
+
+        mergedList[i]=mergedList[i]*2
+
+    return mergedList
+
+
+def getListsHere(listOrder):
+
+    yourList=[]
+    listLenght=int(input("How many elements will the {0}.list be containing?".format(listOrder)))
+
+    for i in range(0,listLenght):
+        listElement = int(input("Enter the {0}.element!!!".format(i)))
+        yourList.append(listElement)
+
+    return yourList
+
+def printOurNewList(latelyUpdatedList):
+
+    for i in range(0,len(latelyUpdatedList)):
+        print("Order in the List: {0} Value:{1} and Type{2}".format(i,latelyUpdatedList[i],type(latelyUpdatedList[i])))
+
+
+if __name__ == '__main__':
+
+    mainList1,mainList2=[], []
+
+    mainList1=getListsHere(1)
+    mainList2 = getListsHere(2)
+
+    mergedList,multipliedList= [], []
+
+    mergedList=mergeListsHere(mainList1,mainList2)
+
+    multipliedList= multiplyMergedList(mergedList)
+
+    printOurNewList(multipliedList)
